01fd7abddffd52bbee73f74f5d2bb8d0c3e40858
andrezito <134400745+andrezito007@users.noreply.github.com>
2024-09-05 12:10:10 -0300

    Add files via upload

---
+++ modelos/medicamento.py
@@ -0,0 +1,12 @@
+class Medicamento:
+    nome = ''
+    tarja = ''
+    local = ''
+    ativo = False
+
+medicamento_tadalafila = Medicamento()
+medicamento_buscopan = Medicamento()
+
+medicamentos = [medicamento_tadalafila, medicamento_buscopan]
+
+print(medicamentos)

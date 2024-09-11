296d5d32fc35fef0364a0e6df2a2fbd698d81352
andrezito <134400745+andrezito007@users.noreply.github.com>
2024-09-11 11:54:30 -0300

    Add files via upload

--- modelos/medicamento.py
+++ modelos/medicamento.py
@@ -1,12 +1,19 @@
 class Medicamento:
     nome = ''
+    nome_científico = ''
     tarja = ''
     local = ''
     ativo = False
 
 medicamento_tadalafila = Medicamento()
 medicamento_buscopan = Medicamento()
+medicamento_paracetamol = Medicamento()
+
+medicamento_tadalafila.nome = 'tadalafila'
+medicamento_tadalafila.nome_científico = 'Cialis'
 
 medicamentos = [medicamento_tadalafila, medicamento_buscopan]
 
 print(medicamentos)
+print(medicamento_tadalafila)
+print(dir(medicamento_tadalafila))
